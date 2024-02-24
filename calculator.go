b12498c365d002a898e128c0e05e5f3fbd271d0a
Simon Ågren <simon.agren@gmail.com>
2024-02-24 13:36:34 +0100

    return error instead of panic

--- calculator.go
+++ calculator.go
@@ -7,69 +7,81 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-func sizeInBytes(av *types.AttributeValue) int {
+func sizeInBytes(av *types.AttributeValue) (int, error) {
 	if av == nil {
-		return 0
+		return 0, nil
 	}
 
 	switch _av := (*av).(type) {
 	case *types.AttributeValueMemberS:
-		return len([]byte(_av.Value))
+		return len([]byte(_av.Value)), nil
 	case *types.AttributeValueMemberN:
-		return len([]byte(_av.Value))
+		return len([]byte(_av.Value)), nil
 	case *types.AttributeValueMemberB:
-		return len(_av.Value)
+		return len(_av.Value), nil
 	case *types.AttributeValueMemberBOOL, *types.AttributeValueMemberNULL:
-		return 1
+		return 1, nil
 	case *types.AttributeValueMemberBS:
 		size := 0
 		for _, v := range _av.Value {
 			size += len(v)
 		}
-		return size
+		return size, nil
 	case *types.AttributeValueMemberL:
 		size := 3
 		for _, v := range _av.Value {
-			size += sizeInBytes(&v)
+			s, err := sizeInBytes(&v)
+			if err != nil {
+				return 0, err
+			}
+			size += s
 			size++
 		}
-		return size
+		return size, nil
 	case *types.AttributeValueMemberM:
 		size := 3
 		for k, v := range _av.Value {
 			size += len([]byte(k))
-			size += sizeInBytes(&v)
+			s, err := sizeInBytes(&v)
+			if err != nil {
+				return 0, err
+			}
+			size += s
 			size++
 		}
-		return size
+		return size, nil
 
 	case *types.AttributeValueMemberNS:
 		size := 0
 		for _, v := range _av.Value {
 			size += len([]byte(v))
 		}
-		return size
+		return size, nil
 	case *types.AttributeValueMemberSS:
 		size := 0
 		for _, v := range _av.Value {
 			size += len(v)
 		}
-		return size
+		return size, nil
 	default:
-		panic(fmt.Sprintf("unknown type %T", _av))
+		return 0, fmt.Errorf("unknown type: %T", _av)
 	}
 }
 
-func StructSizeInBytes(s interface{}) int {
+func StructSizeInBytes(s interface{}) (int, error) {
 	av, err := attributevalue.MarshalMap(s)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
 
 	size := 0
 	for k, v := range av {
 		size += len([]byte(k))
-		size += sizeInBytes(&v)
+		s, err := sizeInBytes(&v)
+		if err != nil {
+			return 0, err
+		}
+		size += s
 	}
-	return size
+	return size, nil
 }
