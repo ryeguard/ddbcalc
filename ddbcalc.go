91f980551ccaeb157fee826fb869de759ac012b2
Simon Rygård <simon.rygard+dev.github@gmail.com>
2024-04-03 21:32:56 +0200

    refactor: split into internal pkg

---
+++ ddbcalc.go
@@ -0,0 +1,34 @@
+package ddbcalc
+
+import (
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/ryeguard/ddbcalc/internal/calc"
+)
+
+const SizeLimitInBytes = 400_000 // 400 KB
+
+// MapSizeInBytes returns the size of a map of AttributeValue in bytes.
+func MapSizeInBytes(m map[string]types.AttributeValue) (int, error) {
+	size := 0
+	for k, v := range m {
+		size += len([]byte(k))
+		s, err := calc.SizeInBytes(&v)
+		if err != nil {
+			return 0, err
+		}
+		size += s
+	}
+	return size, nil
+}
+
+// StructSizeInBytes returns the size of a struct in bytes.
+// It is a convenience function as an alternative to first calling attributevalue.MarshalMap and then MapSizeInBytes.
+func StructSizeInBytes(s interface{}) (int, error) {
+	av, err := attributevalue.MarshalMap(s)
+	if err != nil {
+		return 0, err
+	}
+
+	return MapSizeInBytes(av)
+}
